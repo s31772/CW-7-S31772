d0173af17da2d78edbada77cc0f35c6e77e9a608
s31772 <s31772@pjwstk.edu.pl>
2025-05-04 16:37:37 +0200

    second commit

--- apbd_cw7/apbd_cw7/Models/DTOs/ClientTripGetDTO.cs
+++
@@ -1,9 +0,0 @@
-// namespace apbd_cw7.Models.DTOs;
-//
-// public class ClientTripGetDTO
-// {
-//     public int IdTrip { get; set; }
-//     public int IdClient { get; set; }
-//     public String RegisteredAt { get; set; }
-//     public String PaymentDate { get; set; }
-// }
--- apbd_cw7/apbd_cw7/Models/DTOs/CountryGetDTO.cs
+++
@@ -1,7 +0,0 @@
-// namespace apbd_cw7.Models.DTOs;
-//
-// public class CountryGetDTO
-// {
-//     public int IdCountry { get; set; }
-//     public string Name { get; set; }
-// }
